7dc26567e01b9dc57623daa21b5a8d3e1d545248
Keith Mannock <339082+keithmannock@users.noreply.github.com>
2024-06-27 10:25:53 +0100

    Init of new form

--- students/studentprojects/files/stacks_in_11_3_logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$cookie = str_replace(" ","",'pagesafe');
-	$reload = $cookie.'-reload';
-	header('HTTP/1.1 200 Ok');
-	setcookie($cookie,'',1,'/');
-	setcookie($reload,'',1,'/');
-	exit;
-?>
